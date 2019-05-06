b11ae456ced934c362d715cc23b1c933b7d13d3d
Mckay <mckaywrigley@gmail.com>
2019-05-06 09:56:20 -0700

    Registers user

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,22 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
+
+const Users = require("../data/helpers/Users");
 
 const router = express.Router();
 
-router.post("/register", (req, res) => {});
+router.post("/register", (req, res) => {
+  let user = req.body;
+  let hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+  Users.create(user)
+    .then(user => {
+      return res.json(201).json(user);
+    })
+    .catch(err => {
+      return res.json(500).json({ error: "Could not register the user." });
+    });
+});
 
 router.post("/login", (req, res) => {});
 
